71df0df790cbd0a3ba97ba26df4c0d18c9a55165
Sebastian Mattar <sebastian.mattar@gmail.com>
2020-05-17 17:16:34 +0200

    WIP

--- ledfx/effects/Scope(reactive).py
+++ ledfx/effects/Scope(reactive).py
@@ -3,7 +3,7 @@ from ledfx.color import COLORS
 from PIL import Image, ImageDraw
 import voluptuous as vol
 import numpy as np
-
+import math 
 
 class Strobe(AudioReactiveEffect):
     NAME = "Scope"
@@ -15,15 +15,21 @@ class Strobe(AudioReactiveEffect):
 
     def audio_data_updated(self, data):
         samples = data.audio_sample(raw=True)
+
+        # Get curve from sample data
+        dx = len(samples)/self._dimensions[0]
+        arr = []
+        for x in range(0, self._dimensions[0]):
+            arr.append(np.mean(samples[int(x*dx):int((x+1)*dx)]))
+
+        # determine scaling factor
+        dy = self._dimensions[1] / max(max(map(abs, arr)), 0.1) / 2
+
+        # draw curve
         image = Image.new("RGB", self._dimensions, color=0)
         img1 = ImageDraw.Draw(image)
+        for x in range(1, self._dimensions[0]):
+            img1.line(((x-1, int(arr[x-1]*dy+self._dimensions[1]/2)), (x, arr[x]*dy+self._dimensions[1]/2)), width=1)
 
-        dx = len(samples)/self._dimensions[0]
-        py = 0
-        for x in range(0, self._dimensions[0]):
-            v = np.mean(samples[int(x*dx):int((x+1)*dx)])
-            y = int(self._dimensions[1]/2+v*self._dimensions[1]/2)
-            if x > 0:
-                img1.line(((x-1, py), (x, y)), width=1)
-            py = y
-        self.image=image
+        # output image
+        self.image = image
