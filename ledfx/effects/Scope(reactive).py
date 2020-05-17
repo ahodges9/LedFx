373d9283165e2f21e50760df7d30b5f33a4d4733
Sebastian Mattar <sebastian.mattar@gmail.com>
2020-05-17 17:23:20 +0200

    WIP

--- ledfx/effects/Scope(reactive).py
+++ ledfx/effects/Scope(reactive).py
@@ -1,14 +1,17 @@
 from ledfx.effects.audio import AudioReactiveEffect
 from ledfx.color import COLORS
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFilter, ImageEnhance
 import voluptuous as vol
 import numpy as np
-import math 
+import math
+
 
 class Strobe(AudioReactiveEffect):
     NAME = "Scope"
     CONFIG_SCHEMA = vol.Schema({
     })
+
+    _prevImage = None
 
     def config_updated(self, config):
         return
@@ -24,12 +27,24 @@ class Strobe(AudioReactiveEffect):
 
         # determine scaling factor
         dy = self._dimensions[1] / max(max(map(abs, arr)), 0.1) / 2
-
+        
+        image = None
+        
         # draw curve
-        image = Image.new("RGB", self._dimensions, color=0)
+        if self._prevImage != None:
+            image = self._prevImage
+            enhancer = ImageEnhance.Brightness(image)
+            image = enhancer.enhance(0.9)
+            image = image.filter(ImageFilter.GaussianBlur(radius=1))
+        else:
+            image = Image.new("RGB", self._dimensions, color=0)
+
         img1 = ImageDraw.Draw(image)
         for x in range(1, self._dimensions[0]):
-            img1.line(((x-1, int(arr[x-1]*dy+self._dimensions[1]/2)), (x, arr[x]*dy+self._dimensions[1]/2)), width=1)
+            img1.line(((x-1, int(arr[x-1]*dy+self._dimensions[1]/2)),
+                       (x, arr[x]*dy+self._dimensions[1]/2)), width=1)
+
+        self._prevImage = image
 
         # output image
         self.image = image
