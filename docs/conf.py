27bb938e88bcf8d77f1392dfe9da767861c39fb5
thatdonfc <thatdonfc+git@gmail.com>
2020-12-13 09:58:48 -0800

    Fix conf.py imports

--- docs/conf.py
+++ docs/conf.py
@@ -17,10 +17,10 @@ import sys
 
 import sphinx_rtd_theme
 
+sys.path.insert(0, os.path.abspath(".."))
+
 import ledfx.consts as const
 from ledfx.consts import PROJECT_NAME, PROJECT_VERSION
-
-sys.path.insert(0, os.path.abspath(".."))
 
 # -- Project information -----------------------------------------------------
 
